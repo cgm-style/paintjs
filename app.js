8f87a22b1706335c0711e66d4db41742d927716d
cgm-style <84002015+cgm-style@users.noreply.github.com>
2021-06-02 19:01:31 +0900

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ const canvas = document.querySelector("#jsCanvas"),
       selectColor = document.querySelector("#jsColorSelect"),
       SaveColor = document.querySelector("#jsSaveColor"),
       canvasClear = document.querySelector("#jsClear"),
-      sizeForm = document.querySelector("#jsSize");
+      sizeForm = document.querySelector("#jsSize"),
+      Dot = document.querySelector("#jsDot"),
+      DotPoint = document.querySelector(".DotPoint");
 
-      console.dir(selectColor);
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
@@ -23,7 +24,8 @@ canvas.height = CANVAS_SIZE;
       ctx.lineWidth = 2.5;
 
 let painting = false;
-let filling = false;
+let filling = false,
+    doting = false;
 
 function stopPainting() {
     painting = false;
@@ -44,6 +46,57 @@ function onMouseMove(event) {
         ctx.stroke();
     }
 }
+function handleDotMove()    {
+    if(doting === false)    {
+    handleCanvasClear();
+    stopPainting();
+    painting = false;
+    filling = false;
+    doting = true;
+
+    let dotPoint = document.createElement("div");
+        
+
+    DotPoint.appendChild(dotPoint);
+
+    dotPoint.id = "dotPointer";
+    dotPoint.style.border = `1px solid ${ctx.strokeStyle}`;
+    dotPoint.style.position = `relative`;
+    dotPoint.style.boxSizing = `border-box`;
+
+    dotBoxMove(dotPoint);
+
+    function dotBoxMove(dotBox) {
+        canvas.addEventListener("mousemove", function(event) {
+            let dotSize = range.value;
+            painting = false;
+            filling = false;
+            const x = event.offsetX;
+            const y = event.offsetY;
+
+            dotPoint.style.width = `${dotSize*5}px`;
+            dotPoint.style.height = `${dotSize*5}px`;
+            dotBox.style.top = `${y-dotSize*2}px`;
+            dotBox.style.left = `${x-dotSize*2.5}px`;
+
+
+            canvas.addEventListener("mousedown", function(event) {
+                let dotSize = range.value;
+
+                const x = event.offsetX;
+                const y = event.offsetY;
+                
+                ctx.fillRect(x-dotSize*2.5, y-dotSize*2, dotSize*5, dotSize*5);
+            })
+        });
+    };
+
+    canvas.addEventListener("mouseup", function() {
+    })
+    }else   {
+        filling = true;
+    }
+}
 
 
 
@@ -53,6 +106,12 @@ function handleRangeChange(event)   {
 }
 
 function handleModeClick(event) {
+    if(doting === true) {
+        doting = false;
+        painting = true;
+        canvas.addEventListener("mousemove", onMouseMove);
+        init();
+    }
     if(filling === true)    {
         filling = false;
         mode.innerText = "Fill";
@@ -130,48 +189,60 @@ function handleCanvasSize(event) {
 
     canvas.style.width = `${changeCanvasSizeWidth}px`;
     canvas.style.height = `${changeCanvasSizeHeight}px`;
+    DotPoint.style.width = `${changeCanvasSizeWidth}px`;
+    DotPoint.style.height = `${changeCanvasSizeHeight}px`;
     canvas.width = changeCanvasSizeWidth;
     canvas.height = changeCanvasSizeHeight;
-      
+
     sizeForm[0].value = "";
     sizeForm[1].value = "";
 }
 
-if(canvas)  {
-    canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", startPainting);
-    canvas.addEventListener("mouseup", stopPainting);
-    canvas.addEventListener("mouseleave", stopPainting);
-    canvas.addEventListener("click", handleCanvasClick);
-};
-
-Array.from(colors).forEach(colors => 
-     colors.addEventListener("click", handleColor)
-);
-
-if(range)   {
-    range.addEventListener("input", handleRangeChange);
-};
-
-if(mode)    {
-    mode.addEventListener("click", handleModeClick);
+function init() {
+    if(canvas)  {
+        if(doting === false)    {
+            canvas.addEventListener("mousemove", onMouseMove);
+        }
+        canvas.addEventListener("mousedown", startPainting);
+        canvas.addEventListener("mouseup", stopPainting);
+        canvas.addEventListener("mouseleave", stopPainting);
+        canvas.addEventListener("click", handleCanvasClick);
+    };
+    
+    Array.from(colors).forEach(colors => 
+         colors.addEventListener("click", handleColor)
+    );
+    
+    if(range)   {
+        range.addEventListener("input", handleRangeChange);
+    };
+    
+    if(mode)    {
+        mode.addEventListener("click", handleModeClick);
+    }
+    
+    if(saveBtn) {
+        saveBtn.addEventListener("click", handleSaveClick);
+    }
+    
+    if(selectColor)    {
+        selectColor.addEventListener("input", handleChngeColor);
+    }
+    
+    if(SaveColor)    {
+        SaveColor.addEventListener("click", handleSaveColor);
+    }
+    
+    if(canvasClear) {
+        canvasClear.addEventListener("click", handleCanvasClear);
+    }
+    
+    if(sizeForm)    {
+        sizeForm.addEventListener("submit", handleCanvasSize);
+    }
+    
+    if(Dot) {
+        Dot.addEventListener("click",handleDotMove);
+    }
 }
-
-if(saveBtn) {
-    saveBtn.addEventListener("click", handleSaveClick);
-}
-
-if(selectColor)    {
-    selectColor.addEventListener("input", handleChngeColor);
-}
-
-if(SaveColor)    {
-    SaveColor.addEventListener("click", handleSaveColor);
-}
-if(canvasClear) {
-    canvasClear.addEventListener("click", handleCanvasClear);
-}
-
-if(sizeForm)    {
-    sizeForm.addEventListener("submit", handleCanvasSize);
-}
+init()
